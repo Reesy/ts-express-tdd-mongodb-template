a9d9fd1be3bde239af246b2566fb94a4fdc50ebf
Reesy <James.rees@outlook.com>
2021-09-01 21:58:46 +0100

    Updated port

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ export class config {
     public static port: number = 8000;
     public static dbuser: string = 'blah';
     public static dbpassword: string = 'blue';
-    public static dbhost: string = 'localhost';
+    public static dbhost: string = '0.0.0.0';
     public static dbport: number = 27017;
     public static dbname: string = "ts_mongo_tdd_users";
     public static dbcollection: string = "users";
