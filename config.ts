af6e6e08ca2d665ac99dad24326c5708ee78a54e
Reesy <James.rees@outlook.com>
2021-09-06 22:39:14 +0100

    Added dotenv to handle reading the .env file, added example properties. I should probably decouple test and build, so I can test the app as a microservice instead

--- config.ts
+++ config.ts
@@ -1,10 +1,38 @@
+
+import { resolve } from "path"
+
+import { config as dotEnvConfig } from "dotenv"
+
+let dotEnvConfigPath: string = resolve(__dirname, "../.env");
+
+dotEnvConfig({ path: dotEnvConfigPath })
+
+if (typeof(process.env.MONGO_ROOT_USER) === "undefined") 
+{
+    throw new Error("MONGO_ROOT_USER environment var is not defined");
+};
+
+if (typeof(process.env.MONGO_ROOT_PASSWORD) === "undefined") 
+{
+    throw new Error("MONGO_ROOT_PASSWORD environment var is not defined");
+};
+
+if (typeof(process.env.MONGO_HOST) === "undefined")
+{
+    throw new Error("MONGO_HOST environment var is not defined");
+}
+
+if (typeof(process.env.MONGO_PORT) === "undefined")
+{
+    throw new Error("MONGO_PORT environment var is not defined");
+}
+
 export class config {
-    public static version: string = '0.0.1';
     public static port: number = 8000;
-    public static dbuser: string = 'blah';
-    public static dbpassword: string = 'blue';
-    public static dbhost: string = 'localhost';
-    public static dbport: number = 27017;
+    public static dbuser: any = process.env.MONGO_ROOT_USER;
+    public static dbpassword: any = process.env.MONGO_ROOT_PASSWORD;
+    public static dbhost: any = process.env.MONGO_HOST;
+    public static dbport: any = process.env.MONGO_PORT;
     public static dbname: string = "ts_mongo_tdd_users";
     public static dbcollection: string = "users";
 }
